805ebcf0e5f5c1809eec9177bf782433a2de50d7
martindejos <martindejos@gmail.com>
2021-06-03 15:22:42 +0200

    cards tailwinds

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -1,44 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-
-
-<div class="flex flex-col">
-        <div class="flex-none w-10 h-15 relative">
-          <img src="{{$event->picture}}" class="absolute object-cover" >
-
-         
+    <div class="container flex items-center justify-center">
+<div class=" rounded overflow-hidden shadow-lg flex flex-col items-center m-5">
+    <img class="w-48" src="{{$event->picture}}" alt="Sunset in the mountains">
+    <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">
+            <h1>{{ $event->title }}</h1>
         </div>
-        <div class="flex-auto p-6">
-            <div class="flex flex-wrap">
-                <h1 class="flex-auto text-xl font-semibold">
-                  {{ $event->title }}
-                </h1>
-                <div class="text-xl font-semibold text-gray-500">
-                  {{ $event->date }}
-                </div>
-
-            </div>
-            <div class="flex items-baseline mt-4 mb-6">
-                <div class="space-x-2 flex">
-                    <p class='overflow-x-hidden'>
-                      {{ $event->description }}</p>
-                </div>
-
-            </div>
-            <div class="flex space-x-3 mb-4 text-sm font-medium">
-                <div class="flex-auto flex space-x-3">
-                    <button class="w-1/2 flex items-center justify-center rounded-md bg-black text-white"
-                        type="submit">Book this event</button>
-
-
-
-                </div>
-                <p class="text-sm text-gray-500">
-                    Assistants <br>
-                    {{ $event->capacity}}
-                </p>
+        <p class="text-gray-700 text-base">
+            {{ $event->description }}
+        </p>
+        <p class="text-lg text-black-500">
+            Assistants: {{ $event->capacity}}
+        </p>
+        <div class="text-xl font-semibold text-gray-500">
+            {{ $event->date }}
+        </div>
+        <div class="flex-auto flex space-x-3 m-5 justify-center">
+            <button class="w-1/2 flex items-center justify-center rounded-md bg-black text-white" type="submit">Book this event</button>
         </div>
     </div>
+    <!-- <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div> -->
+</div>
 
 @endsection
