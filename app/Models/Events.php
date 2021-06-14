a1a128427bbae8f744e79524d008d4b4ed57ea93
snejana88 <stefanova1319@gmail.com>
2021-06-14 14:24:31 +0200

    button high

--- app/Models/Events.php
+++ app/Models/Events.php
@@ -12,9 +12,10 @@ class Events extends Model
         'description' => 'required',
         'picture',
         'date' => 'required',
+       
     ];
 
-    protected $fillable = ['capacity', 'isFavorite', 'title', 'description', 'date'];
+    protected $fillable = ['capacity', 'isFavorite'=>'boolean', 'title', 'description', 'date'];
 
 
     use HasFactory;
