48a72e3e46d4ba214619ae798da94afb806dd78e
martindejos <martindejos@gmail.com>
2021-06-22 23:06:25 +0200

    detalle footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,6 +2,7 @@
 
     <img src="https://i.ibb.co/CbHhPbW/Logo-1.png" style="width: 60px; margin-bottom: 10px;" />
     <h2>© Copyright 2021. All Rights Reserved.</h2>
+    <h2>Developed by Diversxs</h2>
     <h2>More info:</h2><a href="{{ url('aboutUs') }}" class="my-3 block text-dark-300 hover:text-red-600 text-xl font-medium duration-700">About us</a>
 
 </footer>
