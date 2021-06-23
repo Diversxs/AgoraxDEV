fb091b083c37af485775c9fc838a2860399ab528
martindejos <martindejos@gmail.com>
2021-06-23 17:54:59 +0200

    aboutUS

--- resources/views/components/aboutUs.blade.php
+++ resources/views/components/aboutUs.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.app')
 @section('content')
-<img class="h-4/5" src="https://static2.bigstockphoto.com/7/1/1/large1500/117408641.jpg" alt="image">
+
 
     <!-- horizontal card -->
-    <h2 class="font-bold text-xl md:text-3xl mb-2 text-orange-700 text-center">About us</h2>
+    <div class=" inset-0 bg-indigo-900 mix-blend-multiply p-6 mb-4">
+        <div class="flex  justify-center items-center px-2  mx-auto my-auto max-w-7xl md:max-h-2xl md:py-6 flex sm:flex-row flex-col">
+            <div class="flex justify-center items-center flex-col sm:mr-10 mr-0">
+                <h1 class="text-4xl text-white md:text-5xl font-bold content-center p-3">About us</h1>
+                <p class="content-center text-2xl text-gray-100 p-3">
+                    Learn a bit from Agorax developers
+                </p>
+                
+            </div>
+            <div>
+                <img class=" w-full h-full rounded-3xl p-3"
+                    src="https://onlinemasters.ohio.edu/wp-content/uploads/2018/03/the-fundamentals-of-a-human-resource-department-image.jpg"
+                    style="width: 250px;" alt="A computer background">
+            </div>
+        </div>
+    </div>
     <div class="flex md:flex-row flex-col items-center justify-center mt-20mt-5 md: mb-40 justify-center ">
         <div class="p-5 bg-orange-200 flex content-center items-center justify-center">
             <div class="bg-indigo-900 rounded-lg shadow-2xl md:flex flex-col items-center justify-center">
-                <div class="flex content-center items-center justify-center">
-                    <img src="https://i.ibb.co/CbHhPbW/Logo-1.png" alt="Logo-1"
-                        style="width: 80px; margin: 15px; border-radius: 100%" />
+                <div class="flex content-center items-center justify-center mt-3">
+                    <img src="https://media-exp3.licdn.com/dms/image/C4D03AQESBs_1AXX49g/profile-displayphoto-shrink_400_400/0/1614425429443?e=1629936000&v=beta&t=cNMr_ixololDcDRtsIFyuxUHRA57SxeSj_WQo92Wcq0" alt="Logo-1"
+                        style="width: 100px; margin: 15px; border-radius: 100%" />
                 </div>
                 <div class="p-6 flex flex-col content-center items-center justify-center">
                     <h2 class="font-bold text-xl md:text-3xl mb-2 text-white">Leah Calafell</h2>
@@ -24,9 +39,9 @@
         </div>
         <div class="p-5 bg-orange-200 flex content-center items-center justify-center">
             <div class="bg-indigo-900 rounded-lg shadow-2xl md:flex flex-col items-center justify-center">
-                <div class="flex content-center items-center justify-center">
-                    <img src="https://i.ibb.co/CbHhPbW/Logo-1.png" alt="Logo-1"
-                        style="width: 80px; margin: 15px; border-radius: 100%" />
+                <div class="flex content-center items-center justify-center mt-3">
+                    <img src="https://media-exp3.licdn.com/dms/image/C4E03AQEOIzvp4GBOjQ/profile-displayphoto-shrink_400_400/0/1551011872911?e=1629936000&v=beta&t=x6t7SeVed0nesy1bAyFpcRmYvHx4qftvML2wbSnuR4Y" alt="Logo-1"
+                        style="width: 100px; margin: 15px; border-radius: 100%" />
                 </div>
                 <div class="p-6 flex flex-col content-center items-center justify-center">
                     <h2 class="font-bold text-xl md:text-3xl mb-2 text-white">Leah Calafell</h2>
@@ -41,9 +56,9 @@
         </div>
         <div class="p-5 bg-orange-200 flex content-center items-center justify-center">
             <div class="bg-indigo-900 rounded-lg shadow-2xl md:flex flex-col items-center justify-center">
-                <div class="flex content-center items-center justify-center">
-                    <img src="https://i.ibb.co/CbHhPbW/Logo-1.png" alt="Logo-1"
-                        style="width: 80px; margin: 15px; border-radius: 100%" />
+                <div class="flex content-center items-center justify-center mt-3">
+                    <img src="https://media-exp3.licdn.com/dms/image/C4D03AQEqqJ8HZu3Oxw/profile-displayphoto-shrink_400_400/0/1622804419525?e=1629936000&v=beta&t=le5uCbQLUnGG2N_oYL2Cxsh2WdLiTFxamOSTEN2CHKo" alt="Logo-1"
+                        style="width: 100px; margin: 15px; border-radius: 100%" />
                 </div>
                 <div class="p-6 flex flex-col content-center items-center justify-center">
                     <h2 class="font-bold text-xl md:text-3xl mb-2 text-white">Leah Calafell</h2>
@@ -58,9 +73,9 @@
         </div>
         <div class="p-5 bg-orange-200 flex content-center items-center justify-center">
             <div class="bg-indigo-900 rounded-lg shadow-2xl md:flex flex-col items-center justify-center">
-                <div class="flex content-center items-center justify-center">
-                    <img src="https://i.ibb.co/CbHhPbW/Logo-1.png" alt="Logo-1"
-                        style="width: 80px; margin: 15px; border-radius: 100%" />
+                <div class="flex content-center items-center justify-center mt-3">
+                    <img src="https://media-exp3.licdn.com/dms/image/C5603AQFlb3CpMCL-Tg/profile-displayphoto-shrink_400_400/0/1620729876880?e=1629936000&v=beta&t=yb31Li9Z6stACga30KD3xJuvJhWhvbWFXAz671R-jew" alt="Logo-1"
+                        style="width: 100px; margin: 15px; border-radius: 100%" />
                 </div>
                 <div class="p-6 flex flex-col content-center items-center justify-center">
                     <h2 class="font-bold text-xl md:text-3xl mb-2 text-white">Leah Calafell</h2>
@@ -75,7 +90,7 @@
         </div>
         <div class="p-5 bg-orange-200 flex content-center items-center justify-center">
             <div class="bg-indigo-900 rounded-lg shadow-2xl md:flex flex-col items-center justify-center">
-                <div class="flex content-center items-center justify-center">
+                <div class="flex content-center items-center justify-center mt-3">
                     <img src="https://i.ibb.co/CbHhPbW/Logo-1.png" alt="Logo-1"
                         style="width: 80px; margin: 15px; border-radius: 100%" />
                 </div>
